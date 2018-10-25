a5b59dfe7c54de9a72bc3b58efe20a2502f84192
Vasyl <mu.1.chested.09@gmail.com>
2018-10-25 12:58:13 +0300

    import template

---
+++ bliscore.ui/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+import './styles/animate.min.css';
+import './styles/main.css';
+/*import './styles/main.scss';
+import './styles/responsive.scss';
+import './styles/style.scss';*/
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
