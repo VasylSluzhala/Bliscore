b9d2a22be99153a56370f6964e3796da3e2b3474
VasylSluzhala <mu.1.chested.09@gmail.com>
2018-12-03 10:49:34 +0200

    module based structure start

--- bliscore.ui/src/components/app.js
+++ bliscore.ui/src/components/app.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxPromise from 'redux-promise';
 import Header from './Header';
 import ContactUs from './ContactUs';
 import Main from './Main';
 import OurTeam from './OurTeam';
 import Footer from './Footer';
 import Services from './Services';
+import reducers from '../reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Header></Header>
-        <Main></Main>
-        <Services></Services>
-        <OurTeam></OurTeam>
-        <ContactUs></ContactUs>
-        <Footer></Footer>
-      </div>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <div>
+          <Header></Header>
+          <Main></Main>
+          <Services></Services>
+          <OurTeam></OurTeam>
+          <ContactUs></ContactUs>
+          <Footer></Footer>
+        </div>
+      </Provider>
     );
   }
 }
