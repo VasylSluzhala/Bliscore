0439d007c812077fa5a7bcd1cbe5f7a17ddd926e
VasylSluzhala <mu.1.chested.09@gmail.com>
2018-12-27 08:56:02 +0200

    classNames experiments

--- bliscore.ui/src/modules/Header/components/Header/index.js
+++ bliscore.ui/src/modules/Header/components/Header/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Nav,NavItem,Navbar,MenuItem,NavDropdown } from 'react-bootstrap';
+import calssNames from 'classnames';
 
 import Logo from './img/bliscore.png';
 
 class Header extends Component {
+
+  _input;
+
   constructor(props) {
     super(props);
-    this.nav = React.createRef();
+    //this.nav = React.createRef();
   }
 
   isTop = true;
 
   componentDidMount(){
+    //debugger;
+    //this._input.props.className = "navbar wow fadeInDown sticky-header";
     window.addEventListener('scroll', this.handleScroll);
   }
 
@@ -21,20 +27,22 @@ class Header extends Component {
   }
 
   handleScroll = () => {
-    // if(window.scrollY > 0){
-    //   if(this.isTop){
-    //    this.nav.setAttribute("class", "navbar wow fadeInDown sticky-header");
-    //   this.isTop = false;
-    // }
-    // } else {
-    //   this.nav.setAttribute("class", "navbar wow fadeInDown");
-    //   this.isTop = true;
-    // }
+    if(window.scrollY > 0){
+      if(this.isTop){
+       //this._input.setAttribute("class", "navbar wow fadeInDown sticky-header");
+      this.isTop = false;
+    }
+    } else {
+      //this._input.setAttribute("class", "navbar wow fadeInDown");
+      this.isTop = true;
+    }
+    console.log(this.isTop);
   }
 
    render() {
+
      return (  
-       <Navbar bsStyle="none" className="navbar wow fadeInDown" data-wow-delay=".2s" data-wow-duration="1s" ref={this.nav} style={{background:"#1b1b1d"}}>
+       <Navbar bsStyle="none" className={"wow fadeInDown " + (this.isTop?"":"sticky-header") } data-wow-delay=".2s" data-wow-duration="1s" ref={c => (this._input = c)} style={{background:"#1b1b1d"}}>
            <Navbar.Header>
              <Navbar.Brand>
                <a href="#hero" className="site-logo navbar-brand"><img src={Logo} alt="" width="150" className="img-responsive" /></a>
