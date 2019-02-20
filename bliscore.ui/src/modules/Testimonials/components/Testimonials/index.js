7b8701a6edbd013def01b1cbf0da1f6dc1a0312f
Andrew <schepanskiy1994@gmail.com>
2019-02-20 18:29:45 +0200

    Change slider for testimonials

--- bliscore.ui/src/modules/Testimonials/components/Testimonials/index.js
+++ bliscore.ui/src/modules/Testimonials/components/Testimonials/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import _ from 'lodash';
 // import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
-import { Carousel } from 'react-bootstrap';
+//import { Carousel } from 'react-bootstrap';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -37,7 +38,30 @@ class Testimonials extends React.Component {
         }
     ];
 
-    render() {
+    renderTiles() {
+        return _.map(this.testimonials, (person, i) =>
+                <div className="media">
+                    <div className="media-left"><img src={person.Url} alt="author" /></div>
+                    <div className="media-body media-middle">
+                        <p>{person.Text}</p>
+                        <div className="author-post"><strong>{person.Post}</strong></div>
+                        <div className="author-name text-muted"><strong>- {person.FullName}</strong></div>
+                    </div>
+                </div>
+        )
+    }
+  
+     render() {
+        var settings = {
+            autoplay:true,
+            autoplaySpeed:3000,
+           dots: true,
+           infinite: true,
+           speed: 500,
+           slidesToShow: 1,
+           slidesToScroll: 1,
+        };
+
         return (
             <section className="burger dark-template-bg" id="testimonials">
                 <div className="container">
@@ -46,20 +70,11 @@ class Testimonials extends React.Component {
                             <h2 className=" text-center mb5 wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1s">What our clients are saying!</h2>
                             <i className="hudge-quote fa fa-quote-left wow fadeInUp" data-wow-delay=".4s" data-wow-duration=".4s"></i>
 
-                            <Carousel>
-                                {_.map(this.testimonials, (person, i) =>
-                                    <Carousel.Item key={i}>
-                                            <div className="media">
-                                                <div className="media-left"><img src={person.Url} alt="author" /></div>
-                                                <div className="media-body media-middle">
-                                                    <p>{person.Text}</p>
-                                                    <div className="author-post"><strong>{person.Post}</strong></div>
-                                                    <div className="author-name text-muted"><strong>- {person.FullName}</strong></div>
-                                                </div>
-                                            </div>
-                                    </Carousel.Item>
-                                )}
-                            </Carousel>
+                  <div className="row align-item-end">
+                     <Slider className="col-xs-8 col-xs-offset-2 wow fadeInUp" data-wow-delay=".1s" data-wow-duration=".5s" {...settings}>
+                        {this.renderTiles()}
+                     </Slider>
+                  </div>
                             <i className="hudge-quote fa fa-quote-right wow fadeInUp" data-wow-delay=".6s" data-wow-duration=".6s"></i>
                         </div>
                     </div>
